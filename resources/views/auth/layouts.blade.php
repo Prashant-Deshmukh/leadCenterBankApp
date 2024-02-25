0a8abf59aea250af18975af153947ee1ede4a43e
Prashant-Deshmukh <prashantvdeshmukh613@gmail.com>
2024-02-25 10:18:43 +0530

    UI changes

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -2,14 +2,14 @@
 <html>
 
 <head>
-    <title>Custom Auth in Laravel</title>
+    <title>Lead Center Bank</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 <body>
     <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
         <div class="container">
-            <a class="navbar-brand mr-auto" href="#">PositronX</a>
+            <a class="navbar-brand mr-auto" href="#">Lead Center Bank</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
